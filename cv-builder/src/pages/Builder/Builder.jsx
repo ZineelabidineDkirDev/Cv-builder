cbcc7f91059a0b6493c5e63e946555c7afd0d6c6
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2024-01-08 10:42:50 +0100

    Builder upadte

--- cv-builder/src/pages/Builder/Builder.jsx
+++ cv-builder/src/pages/Builder/Builder.jsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import { Row, Col, Tabs, Tab } from 'react-bootstrap';
-import cv from '../../components/ExportData';
-import './Builder.css';
+import React from 'react'
+import {Row, Col, Tabs, Tab} from 'react-bootstrap'
+import cv from '../../components/ExportData'
+import './Builder.css'
 
 const Builder = () => {
   return (
-<<<<<<< HEAD
-    <Row className='container-fluid model-builder'>
-      <h1 className='mb-5'>CV Builder</h1>
-      <Col>
-        <Tabs
-          defaultActiveKey="profile"
-          id="justify-tab-example"
-          className="mb-3 nav nav-tabs"
-          justify
-        >
-          <Tab eventKey="profile" className='model-link' title="Profile Information">
-            <cv.Profiles />
-          </Tab>
-          <Tab eventKey="education" className='model-link' title="Formation/Education">
-            <cv.Educations />
-          </Tab>
-          <Tab eventKey="experiences" className='model-link' title="Experiences">
-            <cv.Experiences />
-          </Tab>
-          <Tab eventKey="skills" className='model-link' title="Skills">
-            <cv.Skills />
-          </Tab>
-        </Tabs>
-      </Col>
-    </Row>
-  );
-};
-=======
    <Row className='container-fluid model-builder mt-5'>
     <HeadingStyle title="Builder your CV" />
     <Col>
@@ -59,6 +31,5 @@ const Builder = () => {
    </Row>
   )
 }
->>>>>>> origin/Zayn
 
-export default Builder;
+export default Builder
