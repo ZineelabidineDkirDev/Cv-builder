dbe08b5232df52a0b1115cd46b078c87fddd83f1
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2024-01-15 10:21:22 +0100

    Update App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 import cv from './components/ExportData'
 import { Routes,Route } from 'react-router-dom'
 import {Row, Container} from 'react-bootstrap'
-<<<<<<< HEAD
 
 
 function App() {
   
 
-=======
-import Routa from './Routa'
-
-function App() {
-const islogin=false
->>>>>>> 33fa63cd1b5979dea20ec8dbbd55c3b5cb7b9ef2
   return (
     <div className='background-overlay-style'>
       <Row>
@@ -25,19 +18,10 @@ const islogin=false
         <Route path="/service" element={<cv.Service/>}/>
         {/* <Route path="/template" element={<cv.Template/>}/> */}
         <Route path="/contact" element={<cv.Contact/>}/>
-<<<<<<< HEAD
         <Route path="/builder" element={<cv.Builder/>}/>
         <Route path="/builder#educations" element={<cv.Educations/>}/>
         <Route path="/builder" element={<cv.Builder/>}/>
         <Route path="/builder" element={<cv.Builder/>}/>
-=======
-        <Route path="/login" element={<cv.Login />} />
-          <Route element={<Routa/>}>
-          <Route path="/builder" element={<cv.Builder/>} />
-        </Route>
-        <Route path="/signup" element={<cv.Signup/>}/>
-        <Route path="/builder#educations" element={<cv.Educations/>}/>
->>>>>>> 33fa63cd1b5979dea20ec8dbbd55c3b5cb7b9ef2
      </Routes>
    
      <cv.Footer/>
